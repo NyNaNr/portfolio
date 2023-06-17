79af5f673d7671a0aaec2ebff838e4ab218bd2d0
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-17 11:08:19 +0900

    「Import `.glsl` shaders」を追加

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+module.exports = {
+  webpack(config, { isServer }) {
+    // Import `svg` files as React components
+    config.module.rules.push({
+      test: /\.svg$/,
+      resourceQuery: { not: [/url/] },
+      use: [{ loader: "@svgr/webpack", options: { svgo: false } }],
+    })
 
-module.exports = nextConfig
+    // Import `.glsl` shaders
+    config.module.rules.push({
+      test: /\.glsl$/,
+      type: "asset/source",
+    })
+
+    return config
+  },
+}
