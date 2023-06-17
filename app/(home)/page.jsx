57198c279b6e54348597626a561440bc28555a44
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-17 21:14:10 +0900

    hemeProvider 導入

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,6 +1,8 @@
 "use client"
 import Image from "next/image"
 import dynamic from "next/dynamic"
+import { useState } from "react"
+import { ThemeProvider } from "../../components/ThemeProvider"
 
 const DisplacementSphere = dynamic(
   () =>
@@ -11,8 +13,15 @@ const DisplacementSphere = dynamic(
 )
 
 export default function Home() {
+  const [themeId, setThemeId] = useState("dark")
+
+  const toggleTheme = () => {
+    setThemeId((currentThemeId) =>
+      currentThemeId === "dark" ? "light" : "dark",
+    )
+  }
   return (
-    <>
+    <ThemeProvider themeId={themeId}>
       <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
         <DisplacementSphere />
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -39,15 +48,8 @@ export default function Home() {
             </a>
           </div>
         </div>
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
+        <div className="z-20 text-white text-9xl font-bold ">
+          <p>Yu Nishioka</p>
         </div>
         <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
           <a
@@ -119,6 +121,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-    </>
+    </ThemeProvider>
   )
 }
