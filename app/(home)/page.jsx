a7154cba7d0610bbf2d9fbdff5fbb95dd6f4d4d0
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-19 23:51:19 +0900

    DarkModeButtonをコンポーネント化してすっきり

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,9 +1,10 @@
 "use client"
-import Image from "next/image"
+
 import dynamic from "next/dynamic"
-import { useState, useEffect, useReducer } from "react"
+import { useState } from "react"
 import { ThemeProvider } from "../../components/ThemeProvider"
 import { Transition } from "@/components/Transition"
+import DarkModeButton from "@/components/DarkModeButton"
 
 const DisplacementSphere = dynamic(
   () =>
@@ -16,37 +17,17 @@ const DisplacementSphere = dynamic(
 export default function Home() {
   const [themeId, setThemeId] = useState("light")
 
-  const toggleTheme = () => {
-    setThemeId((currentThemeId) =>
-      currentThemeId === "dark" ? "light" : "dark",
-    )
-  }
-
-  //これで、CSSがモードに切り替かえられる
-  useEffect(() => {
-    // themeに応じて.darkを追加または削除
-    if (themeId === "dark") {
-      document.documentElement.classList.add("dark")
-    } else {
-      document.documentElement.classList.remove("dark")
-    }
-  }, [themeId])
-
   return (
     <Transition in key={themeId} timeout={3000}>
       {(visible, status) => (
         <ThemeProvider themeId={themeId}>
           <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-            <button
-              className="z-20 dark:text-white text-3xl font-semibold"
-              onClick={toggleTheme}
-            >
-              テーマを切り替え
-            </button>
+            <DarkModeButton setThemeId={setThemeId} themeId={themeId} />
+
             <DisplacementSphere />
 
             <div className="z-20 dark:text-white text-9xl font-bold ">
-              <p>Ayumi Nishioka</p>
+              <p>Yu Nishioka</p>
             </div>
           </main>
         </ThemeProvider>
