5533de9030aa32d8329840ea650d42144c5f5e3b
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-20 15:43:34 +0900

    seContext(themeContext)導入

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,10 +1,12 @@
 "use client"
 
 import dynamic from "next/dynamic"
-import { useState } from "react"
+import { useState, createContext } from "react"
 import { ThemeProvider } from "../../components/ThemeProvider"
 import { Transition } from "@/components/Transition"
 import DarkModeButton from "@/components/DarkModeButton"
+
+export const themeContext = createContext()
 
 const DisplacementSphere = dynamic(
   () =>
@@ -16,20 +18,26 @@ const DisplacementSphere = dynamic(
 
 export default function Home() {
   const [themeId, setThemeId] = useState("light")
+  const value = {
+    themeId,
+    setThemeId,
+  }
 
   return (
     <Transition in key={themeId} timeout={3000}>
       {(visible, status) => (
         <ThemeProvider themeId={themeId}>
-          <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-            <DarkModeButton setThemeId={setThemeId} themeId={themeId} />
+          <themeContext.Provider value={value}>
+            <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+              <DarkModeButton setThemeId={setThemeId} themeId={themeId} />
 
-            <DisplacementSphere />
+              <DisplacementSphere />
 
-            <div className="z-20 dark:text-white text-9xl font-bold ">
-              <p>Yu Nishioka</p>
-            </div>
-          </main>
+              <div className="z-20 dark:text-white text-9xl font-bold ">
+                <p>Yu Nishioka</p>
+              </div>
+            </main>
+          </themeContext.Provider>
         </ThemeProvider>
       )}
     </Transition>
