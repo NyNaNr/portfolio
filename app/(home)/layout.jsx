9bee6d48fe092ce5d3108ccb77d89d00a7571533
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-20 22:46:25 +0900

    整理Transitionをhomeのpageに移動

--- app/(home)/layout.jsx
+++ app/(home)/layout.jsx
@@ -1,7 +1,8 @@
 "use client"
 import { useState, createContext } from "react"
 import { ThemeProvider } from "../../components/ThemeProvider"
-import { Transition } from "@/components/Transition"
+
+import Navbar from "@/components/Navbar/Navbar"
 
 export const themeContext = createContext()
 
@@ -15,17 +16,11 @@ export default function HomeLayout({ children }) {
     <>
       <ThemeProvider themeId={themeId}>
         <themeContext.Provider value={value}>
-          {/*<NavBar　（これはコンポーネントで） /> */}
-          {/*<Home　（これはページで） /> */}
-          <Transition in key={themeId} timeout={3000}>
-            {(visible, status) => (
-              <>
-                <div className="fixed left-0 z-20 bg-slate-600 h-full w-20 dark:bg-blue-300"></div>
+          <Navbar />
+          {/*<Intro /> */}
+          {children}
+          {/*<Intro /> */}
 
-                {children}
-              </>
-            )}
-          </Transition>
           {/*<web紹介 /> */}
           {/*<技術紹介 /> */}
         </themeContext.Provider>
