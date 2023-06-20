8e72bbcc42f863633e1ac000d8d14771d4aaca47
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-20 22:47:51 +0900

    だいぶ構成が固まってきた

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,8 +1,10 @@
 "use client"
 
 import dynamic from "next/dynamic"
-
-import DarkModeButton from "@/components/DarkModeButton"
+import { Transition } from "@/components/Transition"
+import { useContext } from "react"
+import DarkModeButton from "@/components/Navbar/DarkModeButton"
+import { themeContext } from "./layout"
 
 const DisplacementSphere = dynamic(
   () =>
@@ -13,15 +15,20 @@ const DisplacementSphere = dynamic(
 )
 
 export default function Home() {
+  const { themeId, setThemeId } = useContext(themeContext)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-      <DarkModeButton />
+    <Transition in key={themeId} timeout={3000}>
+      {(visible, status) => (
+        <>
+          <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+            <DisplacementSphere />
 
-      <DisplacementSphere />
-
-      <div className="z-20 dark:text-white text-9xl font-bold ">
-        <p>Yu Nishioka</p>
-      </div>
-    </main>
+            <div className="z-20 dark:text-white text-9xl font-bold ">
+              <p>Yu Nishioka</p>
+            </div>
+          </main>
+        </>
+      )}
+    </Transition>
   )
 }
