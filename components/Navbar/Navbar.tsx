396062b8ce890d547684ed4690f050df470ec732
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-21 09:23:11 +0900

    isMobileの使用を中止。tailsindCSSに移行

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,26 +1,19 @@
-import { isMobile } from "@/app/utils/isMobile"
 import DarkModeButton from "./DarkModeButton"
-import { useState, useLayoutEffect } from "react"
 
 export default function Navbar() {
   //SSR対策、初回レンダリングされた際にisMobileが実行され、正しい値がステートに保存される
-  const [isMobileDevice, setIsMobileDevice] = useState(true)
-  useLayoutEffect(() => {
-    setIsMobileDevice(isMobile())
-  }, [])
 
   return (
     <>
-      {isMobileDevice ? (
-        <div></div>
-      ) : (
-        <>
-          <div className="fixed left-0 z-20 bg-slate-600 h-full w-20 dark:bg-blue-300"></div>
-          <div className="flex z-20 absolute top-0 right-0 p-2">
-            <DarkModeButton />
-          </div>
-        </>
-      )}
+      <div className="mobile md:invisible">
+        <div className="fixed right-0 top-0 z-20 bg-slate-600 h-20 w-20 dark:bg-blue-300"></div>
+      </div>
+      <div className="pc invisible md:visible">
+        <div className="fixed left-0 z-20 bg-slate-600 h-full w-20 dark:bg-blue-300"></div>
+        <div className="flex z-20 absolute top-0 right-0 p-2">
+          <DarkModeButton />
+        </div>
+      </div>
     </>
   )
 }
