293617da00d92b3302a78ab3754fa24df3fa14b2
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-21 23:57:49 +0900

    humbergerbuttonダークモードに対応

--- components/Navbar/NavbarForMobile.tsx
+++ components/Navbar/NavbarForMobile.tsx
@@ -1,6 +1,8 @@
 import DarkModeButton from "./DarkModeButton"
 import { useState } from "react"
 
+// hamburgerボタンとかをそれぞれコンポネント化するのもあり。でも、useStateの共有忘れすに
+
 export default function NavbarForMobile() {
   const [openMenu, setOpenMenu] = useState(false)
 
@@ -21,22 +23,22 @@ export default function NavbarForMobile() {
             <div
               className={
                 openMenu
-                  ? "w-8 h-0.5 bg-gray-600 translate-y-2.5 rotate-45 transition duration-500 ease-in-out"
-                  : "w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out"
+                  ? "w-8 h-0.5 bg-gray-600 dark:bg-gray-50 translate-y-2.5 rotate-45 transition duration-500 ease-in-out"
+                  : "w-8 h-0.5 bg-gray-600 dark:bg-gray-50 transition duration-500 ease-in-out"
               }
             />
             <div
               className={
                 openMenu
                   ? "opacity-0 transition duration-500 ease-in-out"
-                  : "w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out"
+                  : "w-8 h-0.5 bg-gray-600 dark:bg-gray-50 transition duration-500 ease-in-out"
               }
             />
             <div
               className={
                 openMenu
-                  ? "w-8 h-0.5 bg-gray-600 -rotate-45 transition duration-500 ease-in-out"
-                  : "w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out"
+                  ? "w-8 h-0.5 bg-gray-600 dark:bg-gray-50 -rotate-45 transition duration-500 ease-in-out"
+                  : "w-8 h-0.5 bg-gray-600 dark:bg-gray-50 transition duration-500 ease-in-out"
               }
             />
           </button>
