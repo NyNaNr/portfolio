3df502702e1306d5e765993496a485b466bf5f80
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-21 23:22:04 +0900

    invisible => hidden

--- components/Navbar/NavbarForPC.tsx
+++ components/Navbar/NavbarForPC.tsx
@@ -2,7 +2,7 @@ import DarkModeButton from "./DarkModeButton"
 export default function NavbarForPC() {
   return (
     <>
-      <div className="invisible md:visible">
+      <div className="hidden md:flex">
         <div className="fixed left-0 z-20 bg-slate-600 h-full w-20 dark:bg-blue-300"></div>
         <div className="flex z-20 absolute top-0 right-0 m-10">
           <DarkModeButton />
