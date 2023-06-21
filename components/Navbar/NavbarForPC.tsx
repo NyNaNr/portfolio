e107396f2b182c294d233273059f42a086ef0931
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-21 19:04:46 +0900

    マージン調整

--- components/Navbar/NavbarForPC.tsx
+++ components/Navbar/NavbarForPC.tsx
@@ -4,7 +4,7 @@ export default function NavbarForPC() {
     <>
       <div className="invisible md:visible">
         <div className="fixed left-0 z-20 bg-slate-600 h-full w-20 dark:bg-blue-300"></div>
-        <div className="flex z-20 absolute top-0 right-0 m-3">
+        <div className="flex z-20 absolute top-0 right-0 m-10">
           <DarkModeButton />
         </div>
       </div>
