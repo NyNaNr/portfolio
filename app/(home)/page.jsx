ab28321f5cd77742c85d2665fdf299150dd8aa0b
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-22 23:22:38 +0900

    名前がnavbarの上に表示される問題未解決

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -23,7 +23,7 @@ export default function Home() {
           <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
             <DisplacementSphere />
 
-            <div className="z-20 dark:text-white text-9xl font-bold ">
+            <div className="z-10 dark:text-white text-9xl font-bold ">
               <p>Yu Nishioka</p>
             </div>
           </main>
