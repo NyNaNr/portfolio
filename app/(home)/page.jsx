e386a0753d897a41ea592c3b51af79a6cfe89039
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-25 17:11:14 +0900

    Yu's portfolioの位置を変更

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -23,7 +23,7 @@ export default function Home() {
           <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
             <DisplacementSphere />
 
-            <div className="z-0 dark:text-white text-3xl md:text-6xl lg:text-9xl font-bold ">
+            <div className="z-0 absolute top-72 dark:text-white text-3xl md:text-6xl lg:text-9xl font-bold ">
               <p>Yu&apos;s Portfolio</p>
             </div>
           </main>
