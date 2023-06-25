a9a37ac9238269cb85bd2962d7dd6db1100537b8
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-25 15:41:40 +0900

    とりあえず横並びに

--- components/Navbar/NavbarForPC.tsx
+++ components/Navbar/NavbarForPC.tsx
@@ -1,9 +1,18 @@
 import DarkModeButton from "./DarkModeButton"
+import NavLink from "./NavLink"
+
 export default function NavbarForPC() {
   return (
     <>
       <div className="hidden md:flex">
-        <div className="fixed left-0 z-20 bg-slate-600 h-full w-20 dark:bg-blue-300"></div>
+        <div className="fixed z-20 bg-transparent border-2 h-full w-50 ">
+          <div className="fixed top-72 flex flex-row-reverse -rotate-90 space-x-10 space-x-reverse text-lg">
+            <NavLink text="About me" />
+            <NavLink text="Works" />
+            <NavLink text="Skills" />
+            <NavLink text="Contact" />
+          </div>
+        </div>
         <div className="fixed top-0 right-0 flex z-20  m-10">
           <DarkModeButton />
         </div>
