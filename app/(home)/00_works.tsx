210d416b07cf9c2ec2fe55a48366b0bbc33a502e
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-26 16:04:16 +0900

    w-screen削除。横幅が広がって横スクロールが必要になるから

--- app/(home)/00_works.tsx
+++ app/(home)/00_works.tsx
@@ -1,14 +1,16 @@
 export default function Works() {
   return (
     <>
-      <div className=" mx-auto w-screen max-w-2xl 2xl:max-w-7xl">
+      <div className="mx-auto w-screen max-w-2xl 2xl:max-w-7xl">
         <h2 className="flex z-0 justify-center mb-5 text-3xl font-medium">
           Works
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-4  ">
           {/* 一行目 */}
 
-          <div className="bg-blue-200 h-32">01</div>
+          <div className="bg-blue-200 h-32">
+            <div className="mx-auto">01</div>
+          </div>
           <div className="bg-red-300">02</div>
           <div className="bg-red-400 h-96">03</div>
           {/* 二行目 */}
@@ -24,18 +26,6 @@ export default function Works() {
 
           {/* 四行名 */}
           <div className="bg-blue-400">11</div>
-        </div>
-        <div className="grid grid-cols-2 gap-4 ">
-          <div className="">01</div>
-          <div className="">02</div>
-          <div>03</div>
-          <div>04</div>
-          <div>05</div>
-          <div>06</div>
-          <div>07</div>
-          <div>08</div>
-          <div>09</div>
-          <div>10</div>
         </div>
       </div>
     </>
