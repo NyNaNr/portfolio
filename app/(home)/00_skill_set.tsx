f0edb942a38b21d25af590e7032b31ce21fbe6cf
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-26 23:06:49 +0900

    first

---
+++ app/(home)/00_skill_set.tsx
@@ -0,0 +1,12 @@
+export default function SkillSet() {
+  return (
+    <>
+      <div className="mx-auto w-screen max-w-md sm:max-w-2xl md:max-w-3xl lg:max-w-4xl 2xl:max-w-7xl">
+        <h2 className="flex z-0 justify-center mb-5 text-3xl font-medium">
+          Works
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-4 mx-5 "></div>
+      </div>
+    </>
+  )
+}
