e0b1943e2f589709e1b2448a3e0e8916428117fa
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-26 10:31:24 +0900

    works追加

--- app/(home)/layout.jsx
+++ app/(home)/layout.jsx
@@ -3,7 +3,8 @@ import { useState } from "react"
 import { ThemeProvider } from "../../components/ThemeProvider"
 import { ThemeContext } from "@/components/themeContext"
 import Navbar from "@/components/Navbar/Navbar"
-import AboutMe from "./about_me"
+import AboutMe from "./00_about_me"
+import Works from "./00_works"
 
 export default function HomeLayout({ children }) {
   const [themeId, setThemeId] = useState("light")
@@ -23,6 +24,7 @@ export default function HomeLayout({ children }) {
             {/* tracking-wider leading-relaxed 見やすい行間調べて調整すべし*/}
             <div className="flex flex-col mx-10 max-w-xl justify-center tracking-wider leading-loose dark:text-white">
               <AboutMe />
+              <Works />
 
               {/*<web紹介 /> */}
               {/*<技術紹介 /> */}
