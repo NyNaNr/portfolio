e4a5f5467370a3e941d947b90124236af5700b11
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-26 22:09:26 +0900

    最大幅細かく設定＆画像読み込み成功

--- app/(home)/00_works.tsx
+++ app/(home)/00_works.tsx
@@ -1,15 +1,25 @@
+import Image from "next/image"
+
 export default function Works() {
   return (
     <>
-      <div className="mx-auto w-screen max-w-2xl 2xl:max-w-7xl">
+      <div className="mx-auto w-screen max-w-md sm:max-w-2xl md:max-w-3xl lg:max-w-4xl 2xl:max-w-7xl">
         <h2 className="flex z-0 justify-center mb-5 text-3xl font-medium">
           Works
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-4 mx-5 ">
           {/* 一行目 */}
 
-          <div className="bg-blue-200 h-32">
-            <div className="mx-auto">01</div>
+          <div className="relative overflow-hidden mb-10">
+            <Image
+              src="/randomNameApp.png"
+              alt="randomNameApp Sample"
+              width={500}
+              height={200}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="shadow-lg"
+            />
+            <div className="flex justify-center">Random Name App</div>
           </div>
           <div className="bg-red-300">02</div>
           <div className="bg-red-400 h-96">03</div>
