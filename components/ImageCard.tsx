50c1d951ad2f0cc4d84b516f65825ebfcfcf30f5
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-26 22:36:16 +0900

    微調整

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -7,16 +7,16 @@ interface ImageCardProps {
 
 const ImageCard: React.FC<ImageCardProps> = ({ src, title }) => {
   return (
-    <div className="relative mb-10 hover:scale-105  transform duration-300 ">
+    <div className="relative mb-10 ">
       <Image
         src={src}
         alt={title}
         width={500}
         height={0}
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        className="shadow-lg"
+        className="shadow-lg hover:scale-105  transform duration-300 "
       />
-      <div className="flex justify-center">{title}</div>
+      <div className="flex justify-center mt-5">{title}</div>
     </div>
   )
 }
