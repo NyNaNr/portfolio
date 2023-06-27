5b7fdccb6ae7ce417e54b849e442c1c5979a046c
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-27 22:55:07 +0900

    スキル作成開始

--- app/(home)/00_skill_set.tsx
+++ app/(home)/00_skill_set.tsx
@@ -5,7 +5,36 @@ export default function SkillSet() {
         <h2 className="flex z-0 justify-center mb-5 text-3xl font-medium">
           My skill set
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-4 mx-5 "></div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mx-5 ">
+          <div className="bg-red-500">
+            {/* コンポーネント化しましょう */}
+            <div className="flex justify-center font-semibold my-3">
+              デザイン・コーディング
+            </div>
+            <div className="flex justify-center my-3">
+              基本的なことは一通りできます。CSSフレームワークはTailwindCSSを使っています。
+            </div>
+            <table>
+              <tr>
+                <th>果物</th>
+                <th>味</th>
+              </tr>
+              <tr>
+                <td>イチゴ</td>
+                <td>甘い</td>
+              </tr>
+              <tr>
+                <td>レモン</td>
+                <td>酸っぱい</td>
+              </tr>
+            </table>
+          </div>
+          <div className="bg-red-100">2</div>
+          <div className="bg-orange-100">3</div>
+          <div className="bg-amber-100">4</div>
+          <div className="bg-yellow-100">5</div>
+          <div className="bg-lime-100">6</div>
+        </div>
       </div>
     </>
   )
