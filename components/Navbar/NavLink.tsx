ad74d7438267f815aabc81d1f111a2deff96e02f
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-28 22:45:36 +0900

    react scroll追加

--- components/Navbar/NavLink.tsx
+++ components/Navbar/NavLink.tsx
@@ -1,16 +1,16 @@
 import React from "react"
+import { Link as Scroll } from "react-scroll"
 
 interface NavLinkProps {
   text: string
+  id: string
 }
 
 //リンク（位置）を受け取れるように改良する
 
-const NavLink: React.FC<NavLinkProps> = ({ text }) => (
+const NavLink: React.FC<NavLinkProps> = ({ text, id }) => (
   <p className="relative group whitespace-nowrap dark:text-white">
-    <a href="#" className="">
-      {text}
-    </a>
+    <Scroll to={id}>{text}</Scroll>
     <span className="absolute -bottom-1 left-0 w-0 h-2 bg-strongCyan transition-all  group-hover:w-full group-active:w-full"></span>
   </p>
 )
