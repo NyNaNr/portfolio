c428d80130d3264aad35ba6c757172be82b0354e
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-28 23:03:01 +0900

    それぞれの高さをh-screenに変更

--- app/(home)/00_works.tsx
+++ app/(home)/00_works.tsx
@@ -4,14 +4,14 @@ import ImageCard from "@/components/ImageCard"
 export default function Works() {
   return (
     <>
-      <div className="mx-auto w-screen max-w-md sm:max-w-2xl md:max-w-3xl lg:max-w-4xl 2xl:max-w-7xl">
+      <div
+        className="mx-auto h-screen w-screen max-w-md sm:max-w-2xl md:max-w-3xl lg:max-w-4xl 2xl:max-w-7xl"
+        id="works"
+      >
         <h2 className="flex z-0 justify-center mb-5 text-3xl font-medium">
           Works
         </h2>
-        <div
-          className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-4 mx-5"
-          id="works"
-        >
+        <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-4 mx-5">
           {/* 一行目 */}
 
           <ImageCard src="/randomNameApp32.png" title="Random Name App" />
