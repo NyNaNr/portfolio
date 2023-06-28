02bf095658af64f5320a930748227dfbd83d1048
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-28 14:01:58 +0900

    first

---
+++ components/SkillSet/ElapsedTime.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+type ElapsedTimeProps = {
+  startDate: Date | string
+}
+
+const ElapsedTime: React.FC<ElapsedTimeProps> = ({ startDate }) => {
+  const now = new Date()
+  const start = new Date(startDate)
+  const diffInMonths =
+    now.getMonth() -
+    start.getMonth() +
+    12 * (now.getFullYear() - start.getFullYear())
+
+  const years = Math.floor(diffInMonths / 12)
+  const months = diffInMonths % 12
+
+  if (years > 0) {
+    return <div>{years} 年</div>
+  } else {
+    return <div>{months} ヶ月</div>
+  }
+}
+
+export default ElapsedTime
