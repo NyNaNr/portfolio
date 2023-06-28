5037d319cfc1a07a1756b646e0048b48c690246f
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-28 22:48:43 +0900

    idを追加

--- components/Navbar/NavbarForPC.tsx
+++ components/Navbar/NavbarForPC.tsx
@@ -7,10 +7,10 @@ export default function NavbarForPC() {
       <div className="hidden lg:flex">
         <div className="fixed z-20 bg-transparent  h-full w-48 ">
           <div className="fixed top-72 -left-28 flex flex-row-reverse -rotate-90 space-x-10 space-x-reverse text-lg font-medium">
-            <NavLink text="About me" />
-            <NavLink text="Works" />
-            <NavLink text="Skills" />
-            <NavLink text="Contact" />
+            <NavLink text="About me" id="about-me" />
+            <NavLink text="Works" id="works" />
+            <NavLink text="Skills" id="skills" />
+            <NavLink text="Contact" id="contact" />
           </div>
         </div>
         <div className="fixed top-0 right-0 flex z-20  m-10">
