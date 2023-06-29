bda31c620a0cef545fad3d122c071e76e7e97975
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-29 09:23:06 +0900

    first

---
+++ app/(home)/00_intro.tsx
@@ -0,0 +1,15 @@
+export default function Intro() {
+  return (
+    <>
+      <div
+        className="mx-auto w-screen max-w-md sm:max-w-2xl md:max-w-3xl lg:max-w-4xl 2xl:max-w-7xl mt-72"
+        id="contact"
+      >
+        <h2 className="flex z-0 justify-center mb-5 text-3xl font-medium">
+          Contact
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-4 mx-5 "></div>
+      </div>
+    </>
+  )
+}
