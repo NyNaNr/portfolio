0239f82dc0c95be165fc46998f6b236894987b4a
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-30 16:19:41 +0900

    コンポーネント成功

---
+++ components/Contact/LabeledInput.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react"
+import { motion } from "framer-motion"
+
+interface Props {
+  type: string
+  name: string
+}
+
+const InputField: React.FC<Props> = ({ type, name }) => {
+  const [isFocused, setIsFocused] = useState(false)
+  const [value, setValue] = useState("")
+
+  const labelVariant = {
+    focus: {
+      scale: 0.7,
+      y: -15,
+      x: 5,
+
+      color: "#2563eb",
+      transition: { duration: 0.2, ease: "linear" },
+    },
+    blur: {
+      scale: 1,
+      y: 10,
+      x: 5,
+
+      color: "#6b7280",
+      transition: { duration: 0.2, ease: "linear" },
+    },
+  }
+
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <div className="p-6  relative">
+          <motion.label
+            htmlFor={name}
+            className="absolute pointer-events-none origin-left transition-transform ease-out duration-200"
+            initial={false}
+            animate={isFocused || value !== "" ? "focus" : "blur"}
+            variants={labelVariant}
+          >
+            {["Email", "Name"].includes(name) ? `Your ${name}` : name}
+          </motion.label>
+          <input
+            id={name}
+            type={type}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            className="w-full p-2 border-b-2 border-gray-400 outline-none text-lg transition-colors duration-200 focus:border-blue-500 bg-transparent"
+          />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default InputField
