628c4742f8d9c8b6a9f4597a4953b296e4e5ca02
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-30 17:50:02 +0900

    Contactフォームの送信ボタンも含め実装完了

--- app/(home)/04_contact.tsx
+++ app/(home)/04_contact.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <>
       <div
-        className="mx-auto w-screen max-w-md sm:max-w-2xl md:max-w-3xl lg:max-w-4xl 2xl:max-w-7xl mt-72"
+        className=" relative mx-auto w-screen max-w-md sm:max-w-2xl md:max-w-3xl lg:max-w-4xl 2xl:max-w-7xl mt-72"
         id="contact"
       >
         <h2 className="flex z-0 justify-center mb-5 text-3xl font-medium">
@@ -13,6 +13,11 @@ export default function Contact() {
         <InputField type="email" name="Email" />
         <InputField type="text" name="Name" />
         <InputField type="text" name="Message" />
+        <input
+          type="submit"
+          className="absolute inset-x-0 mx-32
+          bg-blue-500 py-1 px-3 rounded-md text-white"
+        />
       </div>
     </>
   )
