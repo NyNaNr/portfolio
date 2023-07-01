56f72cec1e6600fb20494a2191e4b37d514664cd
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-01 20:58:55 +0900

    NEXT_PUBLIC_

--- app/(home)/(works)/yu-portfolio/page.tsx
+++ app/(home)/(works)/yu-portfolio/page.tsx
@@ -1,4 +1,7 @@
-const Test = () => {
-  return <> {console.log(process.env.SENDGRID_API_KEY)} </>
+export default function Home() {
+  return (
+    <>
+      <div className="text-3xl">Yu2</div>
+    </>
+  )
 }
-export default Test
