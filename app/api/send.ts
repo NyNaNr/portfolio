f58da3fc4d7372aa7834ef0e9469e08e3076acb9
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-02 10:14:55 +0900

    async削除

--- app/api/send.ts
+++ app/api/send.ts
@@ -1,9 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const sgMail = require("@sendgrid/mail")
     sgMail.setApiKey(process.env.SENDGRID_API_KEY) //SendGridのAPIキー
