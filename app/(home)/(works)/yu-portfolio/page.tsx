4f069c0989ba4bbb862958f9ad9180d1e57e508c
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-02 09:31:02 +0900

    testtest

--- app/(home)/(works)/yu-portfolio/page.tsx
+++ app/(home)/(works)/yu-portfolio/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <>
       <div className="text-3xl">{process.env.NEXT_PUBLIC_SENDGRID_API_KEY}</div>
+      <div className="text-3xl">{process.env.NEXT_PUBLIC_MAIL_TO}</div>
+      <div className="text-3xl">{process.env.NEXT_PUBLIC_MAIL_FROM}</div>{" "}
     </>
   )
 }
