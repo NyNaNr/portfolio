12f20ba9d73cbfc35298c06f5573f98a025bc55c
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-02 09:32:37 +0900

    テストから復元

--- app/(home)/(works)/yu-portfolio/page.tsx
+++ app/(home)/(works)/yu-portfolio/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div className="text-3xl">{process.env.NEXT_PUBLIC_SENDGRID_API_KEY}</div>
-      <div className="text-3xl">{process.env.NEXT_PUBLIC_MAIL_TO}</div>
-      <div className="text-3xl">{process.env.NEXT_PUBLIC_MAIL_FROM}</div>{" "}
+      <div className="text-3xl">Yu2</div>
     </>
   )
 }
