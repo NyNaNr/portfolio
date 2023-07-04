06788f188bb6f13b9d2f1edc13acc8350e7175c6
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-04 12:01:48 +0900

    位置完了

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,14 +1,19 @@
 import React, { useEffect } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons"
 
 export const Footer = () => {
   const today = new Date()
   const year = today.getFullYear()
   return (
-    <div className="flex flex-col justify-center items-center w-full mt-52 border-t-4 border-black dark:border-strongCyan">
+    <div className="flex flex-col justify-center items-center w-full mt-52 border-t-4 border-black dark:border-strongCyan ">
       <div
-        className="flex items-end justify-center h-14 w-48 bg-black dark:bg-strongCyan rounded-lg rounded-t-none"
+        className="relative flex items-end justify-center h-14 w-48 bg-black dark:bg-strongCyan rounded-lg rounded-t-none hover:bg-opacity-75"
         onClick={() => window.scrollTo(0, 0)}
       >
+        <div className="absolute top-0 mt-1">
+          <FontAwesomeIcon icon={faChevronUp} style={{ color: "#ffffff" }} />
+        </div>
         <p className="text-white font-semibold text-sm mb-2 relative bottom-0 text-center dark:text-black">
           Back to Top
         </p>
