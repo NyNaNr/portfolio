56d8d04461077bb74a3a4a54a7113765d64cc82b
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-04 10:55:14 +0900

    フッターに西暦と名前を追加

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,8 +1,12 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 export const Footer = () => {
+  const today = new Date()
+  const year = today.getFullYear()
   return (
-    <div className="bottom-0 w-full bg-strongCyan bg-opacity-40">Footer</div>
+    <div className="flex justify-center bottom-0 w-full bg-strongCyan bg-opacity-40 mt-52">
+      {`©${year} Yu`}
+    </div>
   )
 }
 
