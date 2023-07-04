6c7990f6172a25f78183cc67fa9559d7637115f3
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-04 10:42:27 +0900

    first

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const Footer = () => {
+  return (
+    <div className="bottom-0 w-full bg-strongCyan bg-opacity-40">Footer</div>
+  )
+}
+
+export default Footer
