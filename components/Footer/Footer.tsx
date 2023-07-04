e4ce0c41963f8c786a6da88cbcd61534c6c742f1
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-04 11:34:13 +0900

    Back to Top ボタン作成はじめ

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,8 +4,13 @@ export const Footer = () => {
   const today = new Date()
   const year = today.getFullYear()
   return (
-    <div className="flex justify-center w-full bg-strongCyan bg-opacity-40 mt-52">
-      {`©${year} Yu`}
+    <div className="flex flex-col justify-center items-center w-full mt-52 border-t-4 border-black dark:border-strongCyan">
+      <div className="flex items-end justify-center h-14 w-40 bg-black dark:bg-strongCyan rounded-lg rounded-t-none">
+        <p className="text-white text-sm mb-2 relative bottom-0 text-center">
+          Back to Top
+        </p>
+      </div>
+      <p className="text-center dark:text-white">{`©${year} Yu`}</p>
     </div>
   )
 }
