88295662f2126c3482a7cbe4b9fecba69b441c6c
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-04 11:44:35 +0900

    クリックでページ上部へ

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,8 +5,11 @@ export const Footer = () => {
   const year = today.getFullYear()
   return (
     <div className="flex flex-col justify-center items-center w-full mt-52 border-t-4 border-black dark:border-strongCyan">
-      <div className="flex items-end justify-center h-14 w-40 bg-black dark:bg-strongCyan rounded-lg rounded-t-none">
-        <p className="text-white text-sm mb-2 relative bottom-0 text-center">
+      <div
+        className="flex items-end justify-center h-14 w-48 bg-black dark:bg-strongCyan rounded-lg rounded-t-none"
+        onClick={() => window.scrollTo(0, 0)}
+      >
+        <p className="text-white font-semibold text-sm mb-2 relative bottom-0 text-center dark:text-black">
           Back to Top
         </p>
       </div>
