c4ba3d851e16ec72aaef2e84d129381bf7708a52
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-05 10:29:29 +0900

    NavGlobalLinkに変更

--- components/Navbar/NavGlobalLink.tsx
+++ components/Navbar/NavGlobalLink.tsx
@@ -1,17 +1,17 @@
 import React from "react"
 
-interface NavLinkProps {
+interface NavGlobalLinkProps {
   text: string
   id: string
 }
 
 //リンク（位置）を受け取れるように改良する
 
-const NavLink: React.FC<NavLinkProps> = ({ text, id }) => (
+const NavGlobalLink: React.FC<NavGlobalLinkProps> = ({ text, id }) => (
   <p className="relative group whitespace-nowrap cursor-pointer dark:text-white">
     <a href={`/#${id}`}>{text}</a>
     <span className="absolute -bottom-1 left-0 w-0 h-1 bg-strongCyan transition-all  group-hover:w-full group-active:w-full"></span>
   </p>
 )
 
-export default NavLink
+export default NavGlobalLink
