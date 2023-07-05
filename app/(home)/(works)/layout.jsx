3c61cc2a45f449e19505e2e780491e3f1bdeb2aa
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-05 09:05:36 +0900

    dark:bg-lightを追加

--- app/(home)/(works)/layout.jsx
+++ app/(home)/(works)/layout.jsx
@@ -3,13 +3,15 @@
 export default function WorksLayout({ children }) {
   return (
     <div
-      className="flex flex-col mx-auto w-screen xs:w-11/12 sm:max-w-2xl lg:max-w-3xl rounded-lg mt-24 lg:mt-10 bg-blue-200"
+      className="flex flex-col mx-auto w-screen xs:w-11/12 sm:max-w-2xl lg:max-w-3xl rounded-lg mt-24 lg:mt-10 bg-white dark:bg-light"
       style={{
-        minHeight: "calc(100dvh - 170px)",
+        minHeight: "calc(100dvh - 230px)",
         // フッターの高さを引いた(フッター117px)
       }}
     >
-      <div className="flex-grow py-8 px-4 xs:m-8 bg-light">{children}</div>
+      <div className="flex-grow py-8 px-4 xs:m-8 bg-white dark:bg-light">
+        {children}
+      </div>
     </div>
   )
 }
