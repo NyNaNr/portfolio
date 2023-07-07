1f57d80e01770f3b36718858996f300db98cb325
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-07 21:51:58 +0900

    first

---
+++ config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  siteTitle: "yu's blog",
+  siteUrl: "https://yur-portfolio.vercel.app/",
+  repository: "https://github.com/NyNaNr/portfolio",
+  social: {
+    twitter: "",
+    zenn: "s",
+  },
+  excerptLength: 160,
+} as const
