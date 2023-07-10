e353df10ba195d0725e8a817dcc2e5794e0bf111
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-10 19:01:40 +0900

    first

---
+++ components/ScroolDown/ScroolDown.tsx
@@ -0,0 +1,14 @@
+import "./ScroolDown.css"
+
+export default function ScroolDown() {
+  return (
+    <div className="w-full">
+      <div className="s_container">
+        <div className="chevron"></div>
+        <div className="chevron"></div>
+        <div className="chevron"></div>
+        <span className="text">Scroll down</span>
+      </div>
+    </div>
+  )
+}
