f73fab3c49cbb52c0afcb022f2e16ec5bf852ebc
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-10 10:54:05 +0900

    group-hover適用でよりリンクと分かりやすく

--- components/works/ImageCard.tsx
+++ components/works/ImageCard.tsx
@@ -13,16 +13,16 @@ const ImageCard: React.FC<ImageCardProps> = ({ src, title, href }) => {
   return (
     <>
       <Link href={href}>
-        <div className="relative mb-10 ">
+        <div className="relative mb-10 group">
           <Image
             src={src}
             alt={title}
             width={500}
             height={0}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            className="drop-shadow-xl hover:scale-105  transform duration-300 "
+            className="drop-shadow-xl group-hover:scale-105  transform duration-300 "
           />
-          <div className="flex justify-center hover:text-blue-600 mt-5">
+          <div className="flex justify-center group-hover:text-blue-700 group-hover:underline mt-5">
             {title}
           </div>
         </div>
