e31b81e089a3eaead56e52c9fc411ed500949cbd
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-10 19:10:10 +0900

    スクロール位置で消えるように変更

--- components/ScroolDown/ScroolDown.tsx
+++ components/ScroolDown/ScroolDown.tsx
@@ -1,8 +1,30 @@
+"use client"
+import { useEffect, useState } from "react"
+
 import "./ScroolDown.css"
 
 export default function ScroolDown() {
+  const [isTop, setIsTop] = useState(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY
+      if (currentScrollY > 100) {
+        setIsTop(false)
+      } else {
+        setIsTop(true)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll, { passive: true })
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
   return (
-    <div className="w-full">
+    <div
+      className={`w-full transition-opacity duration-500 ${
+        isTop ? "opacity-100" : "opacity-0"
+      }`}
+    >
       <div className="s_container">
         <div className="chevron"></div>
         <div className="chevron"></div>
