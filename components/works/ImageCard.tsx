67fd225525500a9128416e671caa9b0e07073c47
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-11 21:49:55 +0900

    active:対応 ？？？

--- components/works/ImageCard.tsx
+++ components/works/ImageCard.tsx
@@ -20,9 +20,13 @@ const ImageCard: React.FC<ImageCardProps> = ({ src, title, href }) => {
             width={500}
             height={0}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            className="drop-shadow-xl group-hover:scale-105  transform duration-300 "
+            className="drop-shadow-xl transform duration-200 
+            group-hover:scale-105 group-active:scale-105  "
           />
-          <div className="flex justify-center group-hover:text-blue-700 group-hover:underline mt-5">
+          <div
+            className="flex justify-center mt-5 
+          group-hover:text-blue-700 group-active:text-blue-700 group-hover:underline group-active:underline "
+          >
             {title}
           </div>
         </div>
