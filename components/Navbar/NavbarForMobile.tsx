e16610fa471514a2fc5b1a3266fd2e847fbcdcde
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-12 19:48:18 +0900

    元通りに

--- components/Navbar/NavbarForMobile.tsx
+++ components/Navbar/NavbarForMobile.tsx
@@ -23,11 +23,10 @@ export default function NavbarForMobile() {
             className="header-bg z-10 fixed top-0 w-full h-16 bg-light dark:bg-dark
            md:bg-transparent drop-shadow-md"
           ></div>
-
           <button
             onClick={handleMenuOpen}
             type="button"
-            className={`fixed top-0 right-0 flex justify-center items-center flex-col z-50 my-2 mx-3 md:m-3 w-12 h-12 mr-safe
+            className={`fixed top-0 right-0 flex justify-center items-center flex-col z-50 my-2 mx-3 md:m-3 w-12 h-12
             
             hover:active:bg-gray-300 hover:active:bg-opacity-25
               ${openMenu ? "space-y-1" : "space-y-2"}`}
@@ -54,38 +53,38 @@ export default function NavbarForMobile() {
               }
             />
           </button>
-        </div>
-        <nav
-          className={`fixed z-40 ease-in duration-300 ${
-            openMenu
-              ? "text-left top-0 right-0  w-full bg-slate-50 bg-opacity-50 backdrop-blur-lg dark:bg-black dark:bg-opacity-40"
-              : "top-[-100dvh] w-full"
-          }`}
-          style={{ height: "100dvh" }}
-        >
-          <div className="flex justify-center items-center h-full ">
-            <div className="flex z-40 flex-col mx-16 sm:mx-40 text-center space-y-10 text-3xl">
-              {pathname === "/" ? (
-                <>
-                  <NavHomeLink text="About me" id="about-me" />
-                  <NavHomeLink text="Works" id="works" />
-                  <NavHomeLink text="Skills" id="skills" />
-                  <NavHomeLink text="Contact" id="contact" />
-                </>
-              ) : (
-                <>
-                  <NavGlobalLink text="About me" id="about-me" />
-                  <NavGlobalLink text="Works" id="works" />
-                  <NavGlobalLink text="Skills" id="skills" />
-                  <NavGlobalLink text="Contact" id="contact" />
-                </>
-              )}
+          <nav
+            className={`fixed z-40 ease-in duration-300 ${
+              openMenu
+                ? "text-left top-0 right-0  w-full bg-slate-50 bg-opacity-50 backdrop-blur-lg dark:bg-black dark:bg-opacity-40"
+                : "top-[-100dvh] w-full"
+            }`}
+            style={{ height: "100dvh" }}
+          >
+            <div className="flex justify-center items-center h-full ">
+              <div className="flex z-40 flex-col mx-16 sm:mx-40 text-center space-y-10 text-3xl">
+                {pathname === "/" ? (
+                  <>
+                    <NavHomeLink text="About me" id="about-me" />
+                    <NavHomeLink text="Works" id="works" />
+                    <NavHomeLink text="Skills" id="skills" />
+                    <NavHomeLink text="Contact" id="contact" />
+                  </>
+                ) : (
+                  <>
+                    <NavGlobalLink text="About me" id="about-me" />
+                    <NavGlobalLink text="Works" id="works" />
+                    <NavGlobalLink text="Skills" id="skills" />
+                    <NavGlobalLink text="Contact" id="contact" />
+                  </>
+                )}
+              </div>
             </div>
-          </div>
-          <div className="absolute bottom-0 right-0 m-2">
-            <DarkModeButton />
-          </div>
-        </nav>
+            <div className="absolute bottom-0 right-0 m-2">
+              <DarkModeButton />
+            </div>
+          </nav>
+        </div>
       </div>
     </>
   )
