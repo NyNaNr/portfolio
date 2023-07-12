e5000f6efcee9e21676c51e1110585f21b062139
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-12 15:58:56 +0900

    google analytics 追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,10 @@
 
 import "./globals.css"
 import { Inter } from "next/font/google"
+import Script from "next/script"
+import { Suspense } from "react"
+import { GA_TRACKING_ID } from "../lib/contant"
+import { GoogleAnalyticsScript } from "@/lib/gtag"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,6 +23,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
+        <Script
+          id="gtag-init"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+        />
+        <Suspense fallback={null}>
+          <GoogleAnalyticsScript />
+        </Suspense>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
