31cca8c1bc7a929f6f666908b244ba73f75c3e09
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-14 11:00:31 +0900

    first

---
+++ components/About_me/profile_Links.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+
+const profile_Links = () => {
+  return (
+    <>
+      <FontAwesomeIcon icon={faGithub} />
+      <div>profile_Links</div>
+    </>
+  )
+}
+
+export default profile_Links
