ac7212b9d7c718d18da2ce11e7dd6fe72a5430f1
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-07-15 21:28:58 +0900

    first git only

--- components/About_me/profile_Links.tsx
+++ components/About_me/profile_Links.tsx
@@ -2,13 +2,24 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 
-const profile_Links = () => {
+const Profile_Links = () => {
   return (
     <>
-      <FontAwesomeIcon icon={faGithub} />
-      <div>profile_Links</div>
+      <a
+        href="https://github.com/NyNaNr"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex"
+      >
+        <div className="mr-2">
+          <FontAwesomeIcon icon={faGithub} />
+        </div>
+        <div className="text-blue-600 hover:underline">
+          https://github.com/NyNaNr
+        </div>
+      </a>
     </>
   )
 }
 
-export default profile_Links
+export default Profile_Links
