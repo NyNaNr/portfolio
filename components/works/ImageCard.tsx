82d40eeb0b0b0e1cd36f76cad4dbe2b10eedec19
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-08-05 13:12:59 +0900

    テキスト色濃く。変化時間長く

--- components/works/ImageCard.tsx
+++ components/works/ImageCard.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
 
-// リンクも受け取れるようにする
-
 interface ImageCardProps {
   src: string
   title: string
@@ -20,12 +18,12 @@ const ImageCard: React.FC<ImageCardProps> = ({ src, title, href }) => {
             width={500}
             height={0}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            className="drop-shadow-xl transform duration-200 
+            className="drop-shadow-xl transform duration-500 
             group-hover:scale-105 group-active:scale-105  "
           />
           <div
             className="flex justify-center mt-5 
-          group-hover:text-blue-700 group-active:text-blue-700 group-hover:underline group-active:underline "
+          group-hover:text-blue-800 group-active:text-blue-800 group-hover:underline group-active:underline "
           >
             {title}
           </div>
